9513c3b14f7b735fb754f1ccec31fab143d4ad51
dodo <saad9191h@gmail.com>
2022-05-04 01:17:35 -0400

    created computerPlay() functions that reuturns one of the three choices

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// this function randomply outputs rock paper or scissors
+function computerPlay() {
+    let options = ["rock", "paper", "scissors"]; //declaring an array with the options
 
+    let rand_num = Math.floor(Math.random() * 3); //this generates an integer bw 0(inclussive) and 2(inclussive). "Math.random * 3" genrates a float bw 0(inclussive) and 3(exclussive) and Math.floor() rounds it to the lowest integer
+    return options[rand_num];
+}
+
+console.log(computerPlay());
