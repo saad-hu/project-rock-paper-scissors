25c96816cea9d686e7936e1cc2183841c5eaee7d
dodo <saad9191h@gmail.com>
2022-05-23 06:15:30 -0400

    code works fine uptill now. All the required images are donwloaded. When user selects an option game starts. The card image changes as per selection. The score is shown on each card. Till now unlimited rounds can be played.

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-// this function randomly returns rock paper or scissors
+// this function randomly returns rock paper or scissors. The return value will be computer's move/choice in a round
 function computerPlay() {
     let options = ["rock", "paper", "scissors"]; //declaring an array with the options
 
     let rand_num = Math.floor(Math.random() * 3); //this generates an integer bw 0(inclussive) and 2(inclussive). "Math.random * 3" genrates a float bw 0(inclussive) and 3(exclussive) and Math.floor() rounds it to the lowest integer
     return options[rand_num];
-}
-
-//this function asks the user for their choice and returns the choice. asks the user until their choice is correct. also this function lowercases their choice
-function userChoice() {
-
-    let user_input;
-
-    //using do while loop, if the user does not enter one of the three choices, this loop will alert the user and then ask for an input again until the choice is correct. 
-    do {
-        user_input = prompt("Enter your selection (rock, paper, scissors)");
-        user_input = user_input.toLowerCase(); //converts user string to lowercase to maintain consistency
-
-        if (user_input != "rock" && user_input != "paper" && user_input != "scissors") //if user entered wrong choice
-        {
-            alert("Please enter correct choice!");
-            user_input = 0;
-        }
-        else {
-            return user_input;
-        }
-    }
-    while(user_input == 0);
 }
 
 //this function plays ONE round, and RETURNS(not print) the winner of the round. returns tie if computer and user choice are same
@@ -70,50 +48,112 @@ function playRound(computerChoice, userChoice) {
 }
 
 
-//this function simulates a 5 round game and keeps track of who as won each round. if a round is tie, no point is awarded to each player. 
-function game() 
-{
-    let computerSelection, userSelection, roundWinner, computerWins = 0, userWins = 0, roundCount = 1;
-    while( (computerWins != 5) && (userWins != 5)) //5 round game 
-    {   
-        computerSelection = computerPlay(); //stores computer choice. changes for each round
-        userSelection = userChoice(); //stores user choice. changes for each round
-        console.log(`Computer: ${computerSelection}`);
-        console.log(`You: ${userSelection}`);
-        roundWinner = playRound(computerSelection, userSelection); //stores the winner of the current round
+let userSelection; 
+let computerSelection;
+let roundCount = 1;
+let userWins = 0;
+let computerWins = 0;
+
+const icons = document.querySelectorAll('.icon-box');  //creates a node list of all the (3) icon-box 
+
+//adding an eventListener to each icon-box (user selection box)
+icons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+
+        userSelection = icon.getAttribute('id');   //the id of each icon-box is the name of the selection (rock, paper, or scissors). So we can check what the user has selected by checking the id of the icon-box
+        computerSelection = computerPlay();
+
+        let roundWinner = playRound(computerSelection, userSelection);
+
+        //reference to card images nodes
+        const userCardImg = document.querySelector('#user-card img');
+        const computerCardImg = document.querySelector('#computer-card img');
+
+        //changing card images for each round
+        userCardImg.src = "./images/"+userSelection+"-icon-96x96.png";
+        computerCardImg.src = "./images/"+computerSelection+"-icon-96x96.png";
+
+        //refernce to the result's section round result paragraph
+        const roundResult = document.querySelector('#round-result');
+
+        //reference to score (span) nodes
+        const userCardScore = document.querySelector('#user-score');
+        const computerCardScore = document.querySelector('#computer-score');
 
         //these if else statements give points and display the results of each round
         if(roundWinner == "tie")
         {
-            console.log(`Round ${roundCount}: It's a tie! No point awarded.`);
-            roundCount++;
+            roundResult.textContent = `It's a tie! No points awarded.`;
         }
         else if(roundWinner == "computer")
-        {
+        {   
+            roundResult.textContent =  `You Lose! ${computerSelection} beats ${userSelection}`;
             computerWins++;
-            console.log(`Round ${roundCount}: You Lose! ${computerSelection} beats ${userSelection}`);
-            roundCount++;
+            computerCardScore.textContent = computerWins;
         }
         else
         {
+            roundResult.textContent = `You Won! ${userSelection} beats ${computerSelection}`;
             userWins++;
-            console.log(`Round ${roundCount}: You Won! ${userSelection} beats ${computerSelection}`);
-            roundCount++;
+            userCardScore.textContent = userWins;
         }
-    }
 
-    //displays the final score of the whole game
-    console.log(`Computer Score: ${computerWins}`); 
-    console.log(`Your Score: ${userWins}`);
 
-    if(computerWins > userWins)
-    {
-        console.log(`Game Result: You lost the game!`);
-    }
-    else
-    {
-        console.log(`Game Result: You won the game!`);
-    }
-}
+    })
+})
 
-game();
+
+
+
+
+
+
+
+
+//this function simulates a 5 round game and keeps track of who as won each round. if a round is tie, no point is awarded to each player. 
+// function game() 
+// {
+//     let computerSelection, userSelection, roundWinner, computerWins = 0, userWins = 0, roundCount = 1;
+//     while( (computerWins != 5) && (userWins != 5)) //5 round game 
+//     {   
+//         computerSelection = computerPlay(); //stores computer choice. changes for each round
+//         userSelection = userChoice(); //stores user choice. changes for each round
+//         console.log(`Computer: ${computerSelection}`);
+//         console.log(`You: ${userSelection}`);
+//         roundWinner = playRound(computerSelection, userSelection); //stores the winner of the current round
+
+//         //these if else statements give points and display the results of each round
+//         if(roundWinner == "tie")
+//         {
+//             console.log(`Round ${roundCount}: It's a tie! No point awarded.`);
+//             roundCount++;
+//         }
+//         else if(roundWinner == "computer")
+//         {
+//             computerWins++;
+//             console.log(`Round ${roundCount}: You Lose! ${computerSelection} beats ${userSelection}`);
+//             roundCount++;
+//         }
+//         else
+//         {
+//             userWins++;
+//             console.log(`Round ${roundCount}: You Won! ${userSelection} beats ${computerSelection}`);
+//             roundCount++;
+//         }
+//     }
+
+//     //displays the final score of the whole game
+//     console.log(`Computer Score: ${computerWins}`); 
+//     console.log(`Your Score: ${userWins}`);
+
+//     if(computerWins > userWins)
+//     {
+//         console.log(`Game Result: You lost the game!`);
+//     }
+//     else
+//     {
+//         console.log(`Game Result: You won the game!`);
+//     }
+// }
+
+// game();
